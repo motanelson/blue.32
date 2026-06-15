d4c4e37913e3543d76ec9b903b69dfcc48cb4b56
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-15 23:17:20 +0100

    Add files via upload

--- color.c
+++ color.c
@@ -14,25 +14,25 @@ int main()
 	char sss1="!!!!!!";
 	char *argv0=0x82;
         n=0;
-        cls3();
-        /*
-        while(1){
-            ;
-        }*/
+        cls3(0x20f0);
         return 0;
 }
 #asm
 .globl _cls3
 
 _cls3:
+     mov esi,esp
+     add esi,4
+     seg ss
+     mov dx,[esi]
      push ds
      mov eax,0
      push ax
      pop ds 
      mov ebx,*0xb8000
      mov ecx,*2000
-     mov al,*0x20
-     mov ah,*0x0f
+     mov al,dh
+     mov ah,dl
 _clss:
      mov [ebx],al
      inc ebx
